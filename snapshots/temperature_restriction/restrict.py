ccd9dad0b808022d03a2faaa7dcab73dd9f4af7b
Cryoris <jgacon@phys.ethz.ch>
2018-11-07 08:57:00 +0100

    fix filenames & temp. mask

--- snapshots/temperature_restriction/restrict.py
+++ snapshots/temperature_restriction/restrict.py
@@ -17,21 +17,26 @@ else:
 # Define needed directories
 snaps_dir = "/net/astrogate/export/astrodata/jgacon/filex/snapshots/"
 target_dir = snaps_dir + "snapshot_" + target + "/fits3D/"
-density_fits = target_dir + "density_cube.fits"
-temperature_fits = target_dir + "temperature_cube.fits"
 cubex_dir = snaps_dir + "snapshot_" + target + "/cubex/"
+
+# Target files are of form:
+# snap_002_z009p993_256_256_8_box_density
+density_fits = target_dir + "snap_" + target + "_256_256_8_box_density.fits"
+temperature_fits = target_dir + "snap_" + target + "_256_256_8_box_temp.fits"
 
 # Extract FITS data to 3D numpy array
 density_cube = fits.open(density_fits)[0].data
 temperature_cube = fits.open(temperature_fits)[0].data
 
-# Compute pixels where temperature condition is met 
+# Compute pixels where temperature condition is *not* met 
 T_min = 1e3 
 T_max = 5e4
-T_mask = np.where((T_min < temperature_cube) & (temperature_cube < T_max))
+T_mask = np.where((T_max < temperature_cube) | (temperature_cube < T_min))
 
-# Apply temperature restriction to density cube
+# Apply temperature restriction
 density_cube[T_mask] = 0
+temperature_cube[T_mask] = 0
 
 # Store output
 fits.PrimaryHDU(density_cube).writeto(cubex_dir + "temperature_restricted_density.fits")
+fits.PrimaryHDU(temperature_cube).writeto(cubex_dir + "temperature_restricted.fits")
